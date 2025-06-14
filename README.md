4e1f4da4734590bd38ca6dcc31c6e8691b237747
catroidvania <5m4rt.tr45h@gmail.com>
2025-06-14 13:03:17 -0600

    bugfixes

--- README.md
+++ README.md
@@ -8,11 +8,11 @@ works singleplayer as a client mod or multiplayer when installed serverside (doe
 
 ## Config
 
-| config entry       | description                                        |
-|--------------------|----------------------------------------------------|
-| Enabled            | enable/disable veinmining                          |
-| Max Blocks Mined   | maximum blocks broken per veinmine                 |
-| Mine Diagonals     | blocks break diagonal neighbors                    |
-| Allow Tool Break   | if false stops veinmining at 0 durability          |
-| Proper Tools Only  | only veinmine with the correct tool                |
-| Slow Break         | break 1 block per tick, i just think it looks neat |
+| config entry       | description                                                                     |
+|--------------------|---------------------------------------------------------------------------------|
+| Enabled            | enable/disable veinmining                                                       |
+| Max Blocks Mined   | maximum blocks broken per veinmine                                              |
+| Mine Diagonals     | blocks break diagonal neighbors                                                 |
+| Allow Tool Break   | if false stops veinmining at 0 durability                                       |
+| Proper Tools Only  | only veinmine with the correct tool, if off wont veinmine if nothing would drop |
+| Slow Break         | break 1 block per tick, i just think it looks neat                              |
--- src/main/java/com/catroidvania/meinviner/MeinvinerHandler.java
+++ src/main/java/com/catroidvania/meinviner/MeinvinerHandler.java
@@ -4,6 +4,7 @@ import com.fox2code.foxevents.EventHandler;
 import com.fox2code.foxloader.event.GlobalTickEvent;
 import com.fox2code.foxloader.event.interaction.PlayerBreakBlockEvent;
 import com.fox2code.foxloader.event.interaction.PlayerStartBreakBlockEvent;
+import com.fox2code.foxloader.event.world.WorldTickEvent;
 import com.fox2code.foxloader.launcher.FoxLauncher;
 import net.minecraft.client.Minecraft;
 import net.minecraft.common.block.Block;
@@ -15,6 +16,7 @@ import net.minecraft.common.world.World;
 import net.minecraft.server.MinecraftServer;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class MeinvinerHandler {
@@ -24,12 +26,14 @@ public class MeinvinerHandler {
     public static Meinviner.MeinvinerConfig CONFIG = Meinviner.CONFIG;
     public List<MeinvinerBlocklist> blockCaches = new ArrayList<>();
     public static World WORLD;// = Minecraft.getInstance().theWorld;
-    public Block selectedBlock;
+    public HashMap<EntityPlayer, Block> selectedBlocks = new HashMap<>();
+    //public Block selectedBlock;
 
     @EventHandler
     public void onTick(GlobalTickEvent event) {
         if (!CONFIG.enabled) {
             blockCaches.clear();
+            selectedBlocks.clear();
             return;
         }
 
@@ -48,7 +52,10 @@ public class MeinvinerHandler {
             blockCaches.clear();
             return;
         }
+    }
 
+    @EventHandler
+    public void onWorldTickEvent(WorldTickEvent event) {
         List<MeinvinerBlocklist> copyList = new ArrayList<>(blockCaches);
         for (MeinvinerBlocklist bc : copyList) {
             if (!bc.updateBlocks()) {
@@ -63,9 +70,10 @@ public class MeinvinerHandler {
 
         if (WORLD != null) {
             Block block = Blocks.BLOCKS_LIST[WORLD.getBlockId(event.getX(), event.getY(), event.getZ())];
-            if (event.getEntityPlayer().canHarvestBlock(block)) {
-                selectedBlock = block;
-            }
+            //if (event.getEntityPlayer().canHarvestBlock(block)) {
+                selectedBlocks.put(event.getEntityPlayer(), block);
+                //selectedBlock = block;
+            //}
         }
     }
 
@@ -75,11 +83,17 @@ public class MeinvinerHandler {
 
         EntityPlayer player = event.getEntitySource();
         ItemStack item = event.getHeldItem();
-        if (selectedBlock != null && !player.capabilities.isCreativeMode && player.isSneaking()) {
-            if (Meinviner.CONFIG.correctToolOnly && item != null && item.getItem() instanceof ItemTool && !((ItemTool)item.getItem()).isToolEffectiveOnBlock(selectedBlock)) {
-                return;
+        Block block = selectedBlocks.get(player);
+        if (block != null && !player.capabilities.isCreativeMode && player.isSneaking()) {
+            if (Meinviner.CONFIG.correctToolOnly) {
+                if (item == null) return;
+
+                boolean isTool = item.getItem() instanceof ItemTool;
+                if (!isTool || !((ItemTool)item.getItem()).isToolEffectiveOnBlock(block)) {
+                    return;
+                }
             }
-            blockCaches.add(new MeinvinerBlocklist(WORLD, selectedBlock, event.getEntitySource(), event.getHeldItem(), event.getX(), event.getY(), event.getZ(), CONFIG.maxBlocks - 1));
+            blockCaches.add(new MeinvinerBlocklist(WORLD, block, player, item, event.getX(), event.getY(), event.getZ(), CONFIG.maxBlocks - 1));
         }
     }
 }
